47a50fae01f592bb61d9a7d4b5e1d021b0bb4f68
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2024-04-06 02:47:23 +0700

    adding code comments to utils.ts

--- experiments/ao-connect-experiment/utils.ts
+++ experiments/ao-connect-experiment/utils.ts
@@ -1,19 +1,26 @@
 import Arweave from "arweave";
 import fs from 'fs'
 
+// Initialize Arweave connection
 export const arweave = Arweave.init({
     host: 'arweave.net',
     protocol: 'https',
     port: 443
 })
 
+// Function to generate a new Arweave wallet and save it to a JSON file
 export async function generateWallet() {
+    // Generate a new wallet
     const jwk = await arweave.wallets.generate()
+    // Define the filename for the wallet JSON file
     const walletFileName = 'key.json'
+    // Write the generated wallet to a JSON file
     await fs.writeFileSync(walletFileName, JSON.stringify(jwk))
+    // Return the generated wallet object and the filename
     return { jwk, walletFileName }
 }
 
+// Function to pause execution for a specified number of milliseconds
 export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }

b1a1ebd3db002b81434ef6c6d9052cd06b5ba88c
Atticus <atticus@macbook36gb.local>
2024-04-12 06:05:24 -0600

    chore(ethsig): add createEthSigner with verification logs to utils

--- experiments/ao-connect-experiment/ethereum-signer.ts
+++ experiments/ao-connect-experiment/ethereum-signer.ts
@@ -1,51 +1,33 @@
-import { createDataItemSigner, dryrun, message, result } from "@permaweb/aoconnect";
-import { EthereumSigner } from "arbundles";
-import { DataItem, createData } from "warp-arbundles";
+import { dryrun, message, result, createDataItemSigner } from "@permaweb/aoconnect";
+import { arweave, createArweaveDataItemSigner, createEthereumDataItemSigner, generateWallet } from "./utils";
 
 const privateKey = "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
 
-const signer = new EthereumSigner(privateKey);
-
 const processId = "H_3EirnLXUrdqQ2ouE4zB72biF_XknOd8DKCnEP0I1I"
 
-export function createDataItemEthereumSigner (ethSigner: EthereumSigner) {
-    /**
-     * createDataItem can be passed here for the purposes of unit testing
-     * with a stub
-     */
-    const signer = async ({ data, tags, target, anchor }: any) => {
-        const dataItem = createData(data, ethSigner, { tags, target, anchor })
-        return dataItem.sign(ethSigner)
-          .then(async () => ({
-            id: await dataItem.id,
-            raw: await dataItem.getRaw()
-          }))
-      }
-  
-    return signer
-  }
-
 export async function readANTState() {
-
+const jwk = await arweave.wallets.generate()
     const messageId = await message({
         process: processId,
         tags: [{ name: "Action", value: "Info" }],
-        signer: createDataItemEthereumSigner(signer) as any
+        //signer: createDataItemSigner(jwk) as any
+        // signer: createArweaveDataItemSigner(jwk) as any
+       signer: createEthereumDataItemSigner(privateKey) as any
     })
     const res = await result({
         message: messageId,
         process: processId
     })
-    console.log(`chain message results:`)
-    console.dir(res, { depth: 30 })
+    // console.log(`chain message results:`)
+    // console.dir(res, { depth: 30 })
 
     const dryRead = await dryrun({
         process: processId,
         tags: [{ name: "Action", value: "Info" }],
 
     })
-    console.log(`dry run results:`)
-    console.dir(dryRead, { depth: 30 })
+    // console.log(`dry run results:`)
+    // console.dir(dryRead, { depth: 30 })
 }
 
 readANTState()
--- experiments/ao-connect-experiment/utils.ts
+++ experiments/ao-connect-experiment/utils.ts
@@ -1,5 +1,7 @@
+import { EthereumSigner } from "arbundles";
 import Arweave from "arweave";
 import fs from 'fs'
+import { createData, ArweaveSigner, DataItem } from "warp-arbundles";
 
 export const arweave = Arweave.init({
     host: 'arweave.net',
@@ -17,3 +19,57 @@ export async function generateWallet() {
 export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function createArweaveDataItemSigner (wallet: any) {
+    const signer = async ({ data, tags, target, anchor }: any) => {
+      const arSigner = new ArweaveSigner(wallet)
+      const dataItem = createData(data, arSigner, { tags, target, anchor })
+      
+      const res = await dataItem.sign(arSigner)
+        .then(async () => ({
+          id: await dataItem.id,
+          raw: await dataItem.getRaw()
+        }))
+
+        console.dir({
+            valid: await dataItem.isValid(),
+            signature: await dataItem.signature,
+            owner: await dataItem.owner,
+            tags: await dataItem.tags,
+            id: await dataItem.id,
+            res
+          }, { depth: 2 })
+        return res
+    }
+  
+    return signer
+  }
+
+  export function createEthereumDataItemSigner (pk: string) {
+    /**
+     * createDataItem can be passed here for the purposes of unit testing
+     * with a stub
+     */
+    const ethSigner = new EthereumSigner(pk)
+    const signer = async ({ data, tags, target, anchor }: any) => {
+        const dataItem = createData(data, ethSigner, { tags, target, anchor})
+
+        const res = await dataItem.sign(ethSigner)
+          .then(async (b:Buffer) => ({
+            id: await dataItem.id,
+            raw: await dataItem.getRaw(),
+
+          })).catch((e)=> console.error(e))
+          console.dir({
+            valid: await EthereumSigner.verify(ethSigner.publicKey, await dataItem.getSignatureData(), dataItem.rawSignature),
+            signature: await dataItem.signature,
+            owner: await dataItem.owner,
+            tags: await dataItem.tags,
+            id: await dataItem.id,
+            res
+          }, { depth: 2 })
+      return res
+      }
+  
+    return signer
+  }

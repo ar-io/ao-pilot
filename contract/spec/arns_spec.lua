71fb971c675b89820605423604214b70dcee9641
dtfiedler <dylan@ar.io>
2024-05-10 10:11:23 -0400

    chore(test): update test to call using pcall and check errors

--- contract/spec/arns_spec.lua
+++ contract/spec/arns_spec.lua
@@ -3,16 +3,20 @@ local arns = require("arns")
 local constants = require("constants")
 
 describe("arns", function()
-	local original_clock = os.clock
 	local timestamp = os.clock()
 
-	setup(function() end)
+	-- stub out the global state for these tests
+	before_each(function()
+		_G.Records = {}
+		_G.Balances = {
+			["Bob"] = 5000000,
+		}
+	end)
 
-	teardown(function() end)
-
-	it("adds a record to the global balance object", function()
-		Balances["Bob"] = 5000000
-		local result = arns.buyRecord("test-name", "lease", 1, "Bob", false, timestamp, testProcessId)
+	it("adds validate a lease request and add it to global records object", function()
+		local status, result = pcall(arns.buyRecord, "test-name", "lease", 1, "Bob", false, timestamp, testProcessId)
+		print(result)
+		assert.is_true(status)
 		assert.are.same({
 			purchasePrice = 1500,
 			type = "lease",
@@ -21,23 +25,25 @@ describe("arns", function()
 			startTimestamp = 0,
 			endTimestamp = timestamp + constants.MS_IN_A_YEAR * 1,
 		}, result)
-	end)
-
-	it("should allow you to lease a record", function()
-		local result = arns.buyRecord("test-name-2", "lease", 1, "Bob", false, timestamp, testProcessId)
 		assert.are.same({
-			purchasePrice = 1500,
-			type = "lease",
-			undernameCount = 10,
-			processId = testProcessId,
-			startTimestamp = 0,
-			endTimestamp = timestamp + constants.MS_IN_A_YEAR * 1,
-		}, result)
+			["test-name"] = {
+				purchasePrice = 1500,
+				type = "lease",
+				undernameCount = 10,
+				processId = testProcessId,
+				startTimestamp = 0,
+				endTimestamp = timestamp + constants.MS_IN_A_YEAR * 1,
+			},
+		}, Records)
+		assert.are.same({
+			["Bob"] = 4998500,
+			[_G.ao.id] = 1500,
+		}, Balances)
 	end)
 
-	it("should allow you to permabuy a record", function()
-		Balances["Bob"] = 5000000
-		local result, err = arns.buyRecord("test-permabuy-name", "permabuy", 1, "Bob", false, timestamp, testProcessId)
+	it("should validate a permabuy request and add the record to global state and deduct balance from caller", function()
+		local status, result = pcall(arns.buyRecord, "test-permabuy", "permabuy", 1, "Bob", false, timestamp, testProcessId)
+		assert.is_true(status)
 		assert.are.same({
 			purchasePrice = 3000,
 			type = "permabuy",
@@ -46,10 +52,24 @@ describe("arns", function()
 			startTimestamp = 0,
 			endTimestamp = nil,
 		}, result)
+		assert.are.same({
+			["test-permabuy"] = {
+				purchasePrice = 3000,
+				type = "permabuy",
+				undernameCount = 10,
+				processId = testProcessId,
+				startTimestamp = 0,
+				endTimestamp = nil,
+			},
+		}, Records)
+		assert.are.same({
+			["Bob"] = 4997000,
+			[_G.ao.id] = 3000,
+		}, Balances)
 	end)
 
-	it("should allow you to increase the undername count", function()
-		Records["test-name-4"] = {
+	it('should throw an error if the record already exists', function()
+		Records["test-name"] = {
 			endTimestamp = timestamp + constants.MS_IN_A_YEAR,
 			processId = testProcessId,
 			purchasePrice = 1500,
@@ -57,7 +77,29 @@ describe("arns", function()
 			type = "lease",
 			undernameCount = 10,
 		}
-		local result, err = arns.increaseUndernameCount("Bob", "test-name-4", 50, timestamp)
+		local status, result = pcall(arns.buyRecord, "test-name", "lease", 1, "Bob", false, timestamp, testProcessId)
+		assert.is_false(status)
+    	assert.match("Name is already registered", result)
+	end)
+
+	it('should throw an error if the user does not have enough funds', function()
+		Balances["Bob"] = 0
+		local status, result = pcall(arns.buyRecord, "test-name", "lease", 1, "Bob", false, timestamp, testProcessId)
+		assert.is_false(status)
+		assert.match("Insufficient funds", result)
+	end)
+
+	it("should increase the undername count and properly deduct balance", function()
+		Records["test-name"] = {
+			endTimestamp = timestamp + constants.MS_IN_A_YEAR,
+			processId = testProcessId,
+			purchasePrice = 1500,
+			startTimestamp = 0,
+			type = "lease",
+			undernameCount = 10,
+		}
+		local status, result = pcall(arns.increaseUndernameCount, "Bob", "test-name", 50, timestamp)
+		assert.is_true(status)
 		assert.are.same({
 			endTimestamp = timestamp + constants.MS_IN_A_YEAR,
 			processId = testProcessId,
@@ -66,5 +108,19 @@ describe("arns", function()
 			type = "lease",
 			undernameCount = 60,
 		}, result)
+		assert.are.same({
+			["test-name"] = {
+				endTimestamp = timestamp + constants.MS_IN_A_YEAR,
+				processId = testProcessId,
+				purchasePrice = 1500,
+				startTimestamp = 0,
+				type = "lease",
+				undernameCount = 60,
+			},
+		}, Records)
+		assert.are.same({
+			["Bob"] = 4999937.5,
+			[_G.ao.id] = 62.5,
+		}, Balances)
 	end)
 end)
--- contract/src/arns.lua
+++ contract/src/arns.lua
@@ -9,34 +9,6 @@ Auctions = Auctions or {}
 Reserved = Reserved or {}
 
 
-if not Reserved then
-	Reserved = {}
-	Reserved["gateway"] = {
-		endTimestamp = 1725080400000,
-		target = "QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ",
-	}
-
-	Reserved["help"] = {
-		endTimestamp = 1725080400000,
-		target = "QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ",
-	}
-
-	Reserved["io"] = {
-		endTimestamp = 1725080400000,
-		target = "QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ",
-	}
-
-	Reserved["nodes"] = {
-		endTimestamp = 1725080400000,
-		target = "QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ",
-	}
-
-	Reserved["www"] = {
-		endTimestamp = 1725080400000,
-		target = "QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ",
-	}
-end
-
 -- Needs auctions
 -- Needs demand factor
 
@@ -64,7 +36,7 @@ function arns.buyRecord(name, purchaseType, years, from, auction, timestamp, pro
 	local totalRegistrationFee = utils.calculateRegistrationFee(purchaseType, name, years)
 
 	if not Balances[from] or Balances[from] < totalRegistrationFee then
-		error("Insufficient balance")
+		error("Insufficient funds")
 	end
 
 	if Auctions[name] then
@@ -189,11 +161,36 @@ function arns.getAuction(name)
 	return Auctions[name]
 end
 
+function arns.getAuctions()
+	return Auctions
+end
+
 function arns.getReservedName(name)
 	if Reserved[name] == nil then
 		return nil
 	end
 	return Reserved[name]
+end
+
+function arns.addReservedName(name, details)
+	if Reserved[name] then
+		error("Name is already reserved")
+	end
+
+	if Records[name] then
+		error("Name is already registered")
+	end
+
+	if Auctions[name] then
+		error("Name is in auction")
+	end
+
+	Reserved[name] = details
+	return Reserved[name]
+end
+
+function arns.getReservedNames()
+	return Reserved
 end
 
 return arns

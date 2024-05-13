9fd64723a45ea079c486c749638ba1729a3554a7
dtfiedler <dylan@ar.io>
2024-05-13 08:19:06 -0600

    chore(formatting): lua format the copy/pasta code

--- contract/src/crypto/kdf/pbkdf2.lua
+++ contract/src/crypto/kdf/pbkdf2.lua
@@ -1,121 +1,122 @@
-local Bit = require(".crypto.util.bit");
-local Array = require(".crypto.util.array");
-local Stream = require(".crypto.util.stream");
+local Bit = require(".crypto.util.bit")
+local Array = require(".crypto.util.array")
+local Stream = require(".crypto.util.stream")
 local HMAC = require(".crypto.mac.hmac")
 
-local SHA1 = require(".crypto.digest.sha1");
-local SHA2_256 = require(".crypto.digest.sha2_256");
+local SHA1 = require(".crypto.digest.sha1")
+local SHA2_256 = require(".crypto.digest.sha2_256")
 
-local AND = Bit.band;
-local RSHIFT = Bit.rshift;
+local AND = Bit.band
+local RSHIFT = Bit.rshift
 
 local word2bytes = function(word)
-    local b0, b1, b2, b3;
-    b3 = AND(word, 0xFF); word = RSHIFT(word, 8);
-    b2 = AND(word, 0xFF); word = RSHIFT(word, 8);
-    b1 = AND(word, 0xFF); word = RSHIFT(word, 8);
-    b0 = AND(word, 0xFF);
-    return b0, b1, b2, b3;
+	local b0, b1, b2, b3
+	b3 = AND(word, 0xFF)
+	word = RSHIFT(word, 8)
+	b2 = AND(word, 0xFF)
+	word = RSHIFT(word, 8)
+	b1 = AND(word, 0xFF)
+	word = RSHIFT(word, 8)
+	b0 = AND(word, 0xFF)
+	return b0, b1, b2, b3
 end
 
 local PBKDF2 = function()
+	local public = {}
 
-    local public = {};
+	local blockLen = 16
+	local dKeyLen = 256
+	local iterations = 4096
 
-    local blockLen = 16;
-    local dKeyLen = 256;
-    local iterations = 4096;
+	local salt
+	local password
 
-    local salt;
-    local password;
+	local PRF
 
+	local dKey
 
-    local PRF;
+	public.setBlockLen = function(len)
+		blockLen = len
+		return public
+	end
 
-    local dKey;
+	public.setDKeyLen = function(len)
+		dKeyLen = len
+		return public
+	end
 
+	public.setIterations = function(iter)
+		iterations = iter
+		return public
+	end
 
-    public.setBlockLen = function(len)
-        blockLen = len;
-        return public;
-    end
+	public.setSalt = function(saltBytes)
+		salt = saltBytes
+		return public
+	end
 
-    public.setDKeyLen = function(len)
-        dKeyLen = len
-        return public;
-    end
+	public.setPassword = function(passwordBytes)
+		password = passwordBytes
+		return public
+	end
 
-    public.setIterations = function(iter)
-        iterations = iter;
-        return public;
-    end
+	public.setPRF = function(prf)
+		PRF = prf
+		return public
+	end
 
-    public.setSalt = function(saltBytes)
-        salt = saltBytes;
-        return public;
-    end
+	local buildBlock = function(i)
+		local b0, b1, b2, b3 = word2bytes(i)
+		local ii = { b0, b1, b2, b3 }
+		local s = Array.concat(salt, ii)
 
-    public.setPassword = function(passwordBytes)
-        password = passwordBytes;
-        return public;
-    end
+		local out = {}
 
-    public.setPRF = function(prf)
-        PRF = prf;
-        return public;
-    end
+		PRF.setKey(password)
+		for c = 1, iterations do
+			PRF.init().update(Stream.fromArray(s))
 
-    local buildBlock = function(i)
-        local b0, b1, b2, b3 = word2bytes(i);
-        local ii = {b0, b1, b2, b3};
-        local s = Array.concat(salt, ii);
+			s = PRF.finish().asBytes()
+			if c > 1 then
+				out = Array.XOR(out, s)
+			else
+				out = s
+			end
+		end
 
-        local out = {};
+		return out
+	end
 
-        PRF.setKey(password);
-        for c = 1, iterations do
-            PRF.init()
-                .update(Stream.fromArray(s));
+	public.finish = function()
+		local blocks = math.ceil(dKeyLen / blockLen)
 
-            s = PRF.finish().asBytes();
-            if(c > 1) then
-                out = Array.XOR(out, s);
-            else
-                out = s;
-            end
-        end
+		dKey = {}
 
-        return out;
-    end
+		for b = 1, blocks do
+			local block = buildBlock(b)
+			dKey = Array.concat(dKey, block)
+		end
 
-    public.finish = function()
-        local blocks = math.ceil(dKeyLen / blockLen);
+		if Array.size(dKey) > dKeyLen then
+			dKey = Array.truncate(dKey, dKeyLen)
+		end
 
-        dKey = {};
+		return public
+	end
 
-        for b = 1, blocks do
-            local block = buildBlock(b);
-            dKey = Array.concat(dKey, block);
-        end
+	public.asBytes = function()
+		return dKey
+	end
 
-        if(Array.size(dKey) > dKeyLen) then dKey = Array.truncate(dKey, dKeyLen); end
+	public.asHex = function()
+		return Array.toHex(dKey)
+	end
 
-        return public;
-    end
+	public.asString = function()
+		return Array.toString(dKey)
+	end
 
-    public.asBytes = function()
-        return dKey;
-    end
-
-    public.asHex = function()
-        return Array.toHex(dKey);
-    end
-
-    public.asString = function()
-        return Array.toString(dKey);
-    end
-
-    return public;
+	return public
 end
 
 --- @class Array : table
@@ -128,32 +129,32 @@ end
 --- @param digest? string - The digest algorithm to use (sha1, sha256). Defaults to sha1.
 --- @returns string - The derived key
 local pbkdf2 = function(password, salt, iterations, keyLen, digest)
-    local Digest = nil
-    if digest == "sha1" then
-        Digest = SHA1.SHA1
-    elseif digest == "sha256" then
-        Digest = SHA2_256.SHA2_256
-    elseif digest == nil then
-        Digest = SHA1.SHA1
-    else
-        error("Unsupported algorithm: " .. digest)
-    end
+	local Digest = nil
+	if digest == "sha1" then
+		Digest = SHA1.SHA1
+	elseif digest == "sha256" then
+		Digest = SHA2_256.SHA2_256
+	elseif digest == nil then
+		Digest = SHA1.SHA1
+	else
+		error("Unsupported algorithm: " .. digest)
+	end
 
-    local prf = HMAC.HMAC().setBlockSize(64).setDigest(Digest);
+	local prf = HMAC.HMAC().setBlockSize(64).setDigest(Digest)
 
-    local res = PBKDF2()
-            .setPRF(prf)
-            .setBlockLen(16)
-            .setDKeyLen(keyLen)
-            .setIterations(iterations)
-            .setSalt(salt)
-            .setPassword(password)
-            .finish()
+	local res = PBKDF2()
+		.setPRF(prf)
+		.setBlockLen(16)
+		.setDKeyLen(keyLen)
+		.setIterations(iterations)
+		.setSalt(salt)
+		.setPassword(password)
+		.finish()
 
-    return res
+	return res
 end
 
 return {
-    PBKDF2 = PBKDF2,
-    pbkdf2 = pbkdf2
-};
+	PBKDF2 = PBKDF2,
+	pbkdf2 = pbkdf2,
+}
--- contract/src/crypto/util/stream.lua
+++ contract/src/crypto/util/stream.lua
@@ -1,98 +1,91 @@
-local Queue = require(".crypto.util.queue");
+local Queue = require(".crypto.util.queue")
 
-local Stream = {};
-
+local Stream = {}
 
 Stream.fromString = function(string)
-    local i = 0;
-    return function()
-        i = i + 1;
-        return string.byte(string, i);
-    end
+	local i = 0
+	return function()
+		i = i + 1
+		return string.byte(string, i)
+	end
 end
-
 
 Stream.toString = function(stream)
-    local array = {};
-    local i = 1;
+	local array = {}
+	local i = 1
 
-    local byte = stream();
-    while byte ~= nil do
-        array[i] = string.char(byte);
-        i = i + 1;
-        byte = stream();
-    end
+	local byte = stream()
+	while byte ~= nil do
+		array[i] = string.char(byte)
+		i = i + 1
+		byte = stream()
+	end
 
-    return table.concat(array);
+	return table.concat(array)
 end
-
 
 Stream.fromArray = function(array)
-    local queue = Queue();
-    local i = 1;
+	local queue = Queue()
+	local i = 1
 
-    local byte = array[i];
-    while byte ~= nil do
-        queue.push(byte);
-        i = i + 1;
-        byte = array[i];
-    end
+	local byte = array[i]
+	while byte ~= nil do
+		queue.push(byte)
+		i = i + 1
+		byte = array[i]
+	end
 
-    return queue.pop;
+	return queue.pop
 end
-
 
 Stream.toArray = function(stream)
-    local array = {};
-    local i = 1;
+	local array = {}
+	local i = 1
 
-    local byte = stream();
-    while byte ~= nil do
-        array[i] = byte;
-        i = i + 1;
-        byte = stream();
-    end
+	local byte = stream()
+	while byte ~= nil do
+		array[i] = byte
+		i = i + 1
+		byte = stream()
+	end
 
-    return array;
+	return array
 end
 
-
-local fromHexTable = {};
+local fromHexTable = {}
 for i = 0, 255 do
-    fromHexTable[string.format("%02X", i)] = i;
-    fromHexTable[string.format("%02x", i)] = i;
+	fromHexTable[string.format("%02X", i)] = i
+	fromHexTable[string.format("%02x", i)] = i
 end
 
 Stream.fromHex = function(hex)
-    local queue = Queue();
+	local queue = Queue()
 
-    for i = 1, string.len(hex) / 2 do
-        local h = string.sub(hex, i * 2 - 1, i * 2);
-        queue.push(fromHexTable[h]);
-    end
+	for i = 1, string.len(hex) / 2 do
+		local h = string.sub(hex, i * 2 - 1, i * 2)
+		queue.push(fromHexTable[h])
+	end
 
-    return queue.pop;
+	return queue.pop
 end
 
-
-
-local toHexTable = {};
+local toHexTable = {}
 for i = 0, 255 do
-    toHexTable[i] = string.format("%02X", i);
+	toHexTable[i] = string.format("%02X", i)
 end
 
 Stream.toHex = function(stream)
-    local hex = {};
-    local i = 1;
+	local hex = {}
+	local i = 1
 
-    local byte = stream();
-    while byte ~= nil do
-        hex[i] = toHexTable[byte];
-        i = i + 1;
-        byte = stream();
-    end
+	local byte = stream()
+	while byte ~= nil do
+		hex[i] = toHexTable[byte]
+		i = i + 1
+		byte = stream()
+	end
 
-    return table.concat(hex);
+	return table.concat(hex)
 end
 
-return Stream;
+return Stream
